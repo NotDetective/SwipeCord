544ceb5680b2807434c343cc0fea0bba88e4fde5
Woedroe <118165188+WoutLar@users.noreply.github.com>
2024-06-03 13:35:22 +0200

    demo template completed
    added minimal css but more so that we can see how it may look

--- app.vue
+++ app.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <body>
+    <div class="layout">
+      <div class="sidebar">
+        <nav class="navbar">
+          <NuxtLink to="/">Home</NuxtLink>
+          <br>
+          <NuxtLink to="/login">Log In</NuxtLink>
+        </nav>
+        <div class="content-bar">
+          <slot/>
+        </div>
+      </div>
+
+      <!-- Main content -->
+      <main class="main-content">
+        <NuxtPage />
+      </main>
+
+      <div class="placeholder">
+      <slot/>
+      </div>
+    </div>
+  </body>
 </template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+@import '~/assets/style/index.css';
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Log In</h1>
+    <form>
+
+    </form>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
