ed85de3c8eef23bee8add34afdb4802a5b727cef
Micha <micha-elmans@outlook.com>
2024-06-05 14:42:03 +0200

    idfk any more
    end of day commit

--- pages/login.vue
+++ pages/login.vue
@@ -1,14 +1,56 @@
+<script setup>
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useCurrentUser } from 'vuefire'
+const user = useCurrentUser()
+
+const auth = getAuth()
+let email = ref('micha-elmans@outlook.com')
+let password = ref('password')
+
+// display errors if any
+const error = ref(null)
+function signingRedirect() {
+  signInWithEmailAndPassword(auth, email.value, password.value)
+      .then((userCredential) => {
+        auth.currentUser = userCredential.user;
+        console.log('Signed in', userCredential.user)
+      })
+      .catch((reason) => {
+        console.error('Failed signingRedirect', reason)
+        error.value = reason;
+      });
+}
+</script>
+
 <template>
   <div>
     <h1>Log In</h1>
-    <form>
-
+    <form @submit.prevent="signingRedirect">
+      <label for="email">Email</label>
+      <input
+          type="email"
+          id="email"
+          name="email"
+          v-model="email"
+          required
+      >
+      <label for="password">Password</label>
+      <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          v-model="password"
+      >
+      <button type="submit">Log In</button>
     </form>
   </div>
-</template>
 
-<script setup>
-</script>
+  {{ error }}
+
+  {{ user }}
+
+</template>
 
 <style scoped>
 </style>
