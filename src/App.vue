e40d2538c6ed693ababb3c34d39b4257975b4f7b
Micha <micha-elmans@outlook.com>
2024-06-10 13:00:19 +0200

    made the env work

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+import { useCollection } from 'vuefire'
+import { collection } from 'firebase/firestore'
+
+import { useFirestore } from 'vuefire'
+const db = useFirestore()
+
+const users = useCollection(collection(db, 'users'))
+
+console.log(users)
+
 </script>
 
 <template>
