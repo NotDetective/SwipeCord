6452a3fdac6aa12cac363d0e13e399a80d46612f
Jannus <jan.honing2@gmail.com>
2024-06-10 14:10:00 +0200

    einde dag commit 10/6/2024

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
+import "@/assets/home.css";
 
+("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap");
 </script>
 
 <template>
+<h1>Welkom op Swipecord</h1>
+<div class="box_home">
+  <div class="links">
+    <p>Log in met Google</p>
+    <button type="button" class="login-with-google-btn" >
+      Sign in with Google
+    </button>
+  </div>
+
+  <div class="rechts">
+    <p>Log in</p>
+  </div>
+</div>
 
 </template>
 
