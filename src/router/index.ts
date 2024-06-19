6110bd44d48748e27ae719ffaf1ef5b33e3e07f0
Woedroe <118165188+WoutLar@users.noreply.github.com>
2024-06-19 14:30:20 +0200

    hotfix2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GatchaView from '../views/GatchaView.vue';
-import Home from "@../views/Home.vue";
+import Home from "@/views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
