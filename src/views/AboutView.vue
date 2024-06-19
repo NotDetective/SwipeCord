f382ffd71e084591084d824ed0636ccfa9952075
Jannus <jan.honing2@gmail.com>
2024-06-19 14:25:37 +0200

    sesion information

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <script setup>
+import { getAuth } from "firebase/auth";
 
+const auth = getAuth();
+const user = auth.currentUser;
+
+if (user) {
+  // User is signed in, see docs for a list of available properties
+  // https://firebase.google.com/docs/reference/js/auth.user
+  // ...
+  console.log(user.displayName)
+} else {
+  // No user is signed in.
+  this.$router.push('/')
+}
 
 
 </script>
@@ -7,6 +20,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{user.displayName}}</p>
   </div>
 
 </template>
